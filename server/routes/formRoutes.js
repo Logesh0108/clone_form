7a9e75f00850dacc8d500a25bd78e4486942d831
Logesh <logeshnalliyappan@gmail.com>
2026-06-29 17:47:54 +0530

    First Commit

--- server/routes/formRoutes.js
+++ server/routes/formRoutes.js
@@ -25,13 +25,4 @@ router.post(
   ]),
   createForm
 );
-
-router.get("/", getAllForms);
-
-router.get("/:id", getFormById);
-
-router.put("/:id", updateForm);
-
-router.delete("/:id", deleteForm);
-
 module.exports = router;
